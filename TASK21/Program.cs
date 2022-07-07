aa274ead7a2f88be95cfbb432cc81a6bc7887385
Nadezhda <nbykova79@gmail.com>
2022-07-07 17:01:17 +0300

    save

--- TASK21/Program.cs
+++ TASK21/Program.cs
@@ -1,21 +1,6 @@
 ﻿//принимает на вход координаты 2х точек и находит расстояние между ними в 3D
 // пространстве
-// Console. WriteLine("Введите координаты двух точек в 3хмерном пространстве (x1,y1,z1,x2,y2,z2)");
-// int num = Console.Read();
-// string number = num.ToString();
 
-// string Distance()
-// {
-// int [] array = new int [number.Length];
-// for ( int i=0; i < number.Length; i++)
-//     {
-//       array [i] = int.Parse(number[i].ToString());
-//     }
-// double dist = Math.Sqrt(Math.Pow(array[3]-array[0],2)+ Math.Pow(array[4]-array[1],2)+
-// Math.Pow(array[5]-array[3],2));
-// return $"Расстояние между вашими точками в пространстве равно {dist}";
-// }
-// Console.WriteLine(Distance());
 Console.WriteLine("Введите координату Х первой точки");
 int X1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Y первой точки");
