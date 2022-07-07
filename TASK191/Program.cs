6c1353e62a881c9d338961638618347ea4248225
Nadezhda <nbykova79@gmail.com>
2022-07-07 16:38:10 +0300

    save

---
+++ TASK191/Program.cs
@@ -0,0 +1,23 @@
+﻿//принимает на вход пятизначное число и проверяет, является ли оно полиндромом
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+
+string Polindrom()
+{
+//int [] array = new int [num.Length];
+// for
+//  ( int i=0; i < num.Length; i++)
+//     {
+//       array [i] = int.Parse(num[i].ToString());
+//     }
+char [] array = num.ToCharArray();
+if (array[0] == array[4] & array[1] == array[3])
+{
+
+return "Полиндром";
+}
+else {
+return "Не полиндром";
+}
+}
+Console.WriteLine(Polindrom());
